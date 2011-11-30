b0c151db8287682fac8c68e9493ebb1d88908a34
matt.todd <matt.todd@asolutions.com>
2011-11-30 05:58:22 -0600

    fixed the floor issue with nate young's help

--- maze.cpp
+++ maze.cpp
@@ -2,21 +2,39 @@
 
 void draw_maze()
 {
-   glPushMatrix();
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
    glColor3d(1, 0, 0);
 
-   //glTranslated(0,0,-5);
+   glPushMatrix();
+      glTranslated(-10,1,10);
+      glutSolidSphere(1,16,16);
+   glPopMatrix();
 
-   glutSolidSphere(1,16,16);
+   glPushMatrix();
+      glTranslated(10,1,10);
+      glutSolidSphere(1,16,16);
+   glPopMatrix();
 
-/*
-   glBegin(GL_TRIANGLES);
-      glVertex3f(-10.0f, 0.0f,  10.0f);
-      glVertex3f( 10.0f, 0.0f,  10.0f);
-      glVertex3f( 10.0f, 0.0f, -10.0f);
-      glVertex3f(-10.0f, 0.0f, -10.0f);
-   glEnd();
-*/
+   glPushMatrix();
+      glTranslated(10,1,-10);
+      glutSolidSphere(1,16,16);
+   glPopMatrix();
+
+   glPushMatrix();
+      glTranslated(-10,1,-10);
+      glutSolidSphere(1,16,16);
+   glPopMatrix();
+
+   glColor3d(0.5, 0.5, 0.5);
+   glPushMatrix();
+      glBegin(GL_TRIANGLES);
+         glVertex3f(-10.0f, 0.0f,  10.0f); // 1
+         glVertex3f( 10.0f, 0.0f,  10.0f); // 2
+         glVertex3f( 10.0f, 0.0f, -10.0f); // 3
+
+         glVertex3f(-10.0f, 0.0f,  10.0f); // 1
+         glVertex3f( 10.0f, 0.0f, -10.0f); // 3
+         glVertex3f(-10.0f, 0.0f, -10.0f); // 4
+      glEnd();
    glPopMatrix();
 }
